916f2f6c3672b40aa9a56e41e5262f9edaa210d2
Sysoev, Vladimir <vladimir.sysoev@sarov-itc.ru>
2016-01-08 15:14:03 +0300

    Added reading event from the database

--- current.go
+++ current.go
@@ -3,33 +3,65 @@ package main
 import
 //	"labix.org/v2/mgo"
 (
-	"errors"
+	"encoding/json"
 	"flag"
 	"log"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"labix.org/v2/mgo"
 )
 
-// Datestamp type definition
-type Datestamp int64
+type (
+	// Datestamp type definition
+	Datestamp int64
 
-// ScalarValue stores one scalar value for SystemState
-type ScalarValue struct {
-	VarID     int
-	BoxID     int
-	Value     float32
-	TimePoint Datestamp
-}
+	// ScalarValue stores one scalar value for SystemState
+	ScalarValue struct {
+		VarID     int
+		BoxID     int
+		Value     float32
+		TimePoint Datestamp
+	}
 
-// VectorValue stores one vector for SystemState
-type VectorValue struct {
-	VecID     int
-	BoxID     int
-	TimePoint Datestamp
-}
+	// VectorValue stores one vector for SystemState
+	VectorValue struct {
+		VecID     int
+		BoxID     int
+		TimePoint Datestamp
+	}
 
-// SystemState is type to preserve current system state and track changes with the time
-type SystemState struct {
-	scalar ScalarValue
-}
+	// SystemState is type to preserve current system state and track changes with the time
+	SystemState struct {
+		scalar ScalarValue
+	}
+	// EventReader interface DI for Event reader
+	EventReader interface {
+		Dial(url string) error
+		SetEventStore(dbName string, colName string)
+		ReadEvents(fromId string, outQueue chan string) error
+		Close()
+	}
+	//EventWriter interface DI for event submission to event store
+	EventWriter interface {
+		Dial(url string) error
+		SetEventStore(dbName string, colName string)
+		CommitEvent(eventJSON string) error
+		Close()
+	}
+	// MongoEventReader implements reading events from MongoDB storage
+	MongoEventReader struct {
+		session              *mgo.Session
+		dbName               string
+		eventStoreCollection string
+	}
+	// MongoEventWriter implements event submission to MongoDB storage
+	MongoEventWriter struct {
+		session              *mgo.Session
+		dbName               string
+		eventStoreCollection string
+	}
+)
 
 var sysState SystemState
 
@@ -39,15 +71,99 @@ func loadSystemState(eventSource <-chan string) {
 	sysState.scalar.VarID = 1
 	sysState.scalar.TimePoint = 1
 }
-func eventReader(serverName string) (<-chan string, error) {
-	return nil, errors.New("Not implemented")
+
+// NewMongoEventReader makes new object
+func NewMongoEventReader() EventReader {
+	return &MongoEventReader{}
+}
+
+// Dial connect to server by Mongo URL
+func (e *MongoEventReader) Dial(url string) error {
+	var err error
+	e.session, err = mgo.Dial(url)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// SetEventStore define database name and collection name to store events
+func (e *MongoEventReader) SetEventStore(dbName string, colName string) {
+	e.dbName = dbName
+	e.eventStoreCollection = colName
+}
+
+// ReadEvent Read JSON events started from fromId to slice of strings
+func (e *MongoEventReader) ReadEvents(fromId string, outQueue chan string) error {
+	var (
+		iter   *mgo.Iter
+		result interface{}
+	)
+	if fromId != "" {
+		iter = e.session.DB(e.dbName).C(e.eventStoreCollection).Find(bson.M{"_id": bson.M{"$gt": fromId}}).Iter()
+	} else {
+		iter = e.session.DB(e.dbName).C(e.eventStoreCollection).Find(nil).Iter()
+	}
+	go func() {
+		for iter.Next(&result) {
+			s, err := json.Marshal(result)
+			if err != nil {
+				return
+			}
+			outQueue <- string(s)
+		}
+		close(outQueue)
+	}()
+	return nil
+}
+
+// Close closes connection to the server
+func (e *MongoEventReader) Close() {
+	e.session.Close()
+}
+
+// NewMongoEventWriter make new object and returns pointer to interface
+func NewMongoEventWriter() EventWriter {
+	return &MongoEventWriter{}
+}
+
+// Dial connect to MongoDB storage
+func (e *MongoEventWriter) Dial(url string) error {
+	var err error
+	e.session, err = mgo.Dial(url)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// SetEventStore sets database name and event store collection name to store events
+func (e *MongoEventWriter) SetEventStore(dbName string, colName string) {
+	e.dbName = dbName
+	e.eventStoreCollection = colName
+}
+
+// CommitEvent commits one event to the data store
+func (e *MongoEventWriter) CommitEvent(eventJSON string) error {
+	var object interface{}
+	err := json.Unmarshal([]byte(eventJSON), &object)
+	if err != nil {
+		return err
+	}
+	return e.session.DB(e.dbName).C(e.eventStoreCollection).Insert(object)
+}
+
+// Close closes connection to the MongoDB server
+func (e *MongoEventWriter) Close() {
+	e.session.Close()
 }
 func main() {
 	var dbServer string
 	flag.StringVar(&dbServer, "server", "", "Path to database mongo://localhost")
-	eventRead, err := eventReader(dbServer)
+	realReader := NewMongoEventReader()
+	err := realReader.Dial(dbServer)
 	if err != nil {
 		log.Fatal(err)
 	}
-	loadSystemState(eventRead)
+	//loadSystemState(eventRead)
 }
--- current_test.go
+++ current_test.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+type (
+	PositiveEventReader struct{}
+)
+
+func NewPositiveEventReader() EventReader {
+	return &PositiveEventReader{}
+}
+
+func (e *PositiveEventReader) Dial(url string) error {
+	return nil
+}
+
+func (e *PositiveEventReader) SetEventStore(dbName string, colName string) {
+
+}
+func (e *PositiveEventReader) ReadEvents(fromId string, outQueue chan string) error {
+	return nil
+}
+func (e *PositiveEventReader) Close() {
+	return
+}
+
+func TestJSONUnmarshalling(t *testing.T) {
+	Convey("Simple json unmarshalling", t, func() {
+		simpleJson := "{\"name\":\"value\"}"
+		var objmap interface{}
+		err := json.Unmarshal([]byte(simpleJson), &objmap)
+		So(err, ShouldBeNil)
+	})
+}
 
 func TestCurrentState(t *testing.T) {
 	Convey("1 should equal 1", t, func() {
@@ -17,10 +49,31 @@ func TestCurrentState(t *testing.T) {
 		So(sysState.scalar.VarID, ShouldBeZeroValue)
 		So(sysState.scalar.TimePoint, ShouldBeZeroValue)
 	})
-	Convey("Test eventReader", t, func() {
-		ev, err := eventReader("test")
-		So(ev, ShouldNotBeNil)
+	Convey("Test MongoEventReader", t, func() {
+		mng := NewMongoEventReader()
+		So(mng, ShouldNotBeNil)
+		err := mng.Dial("mongodb://127.0.0.1/test")
 		So(err, ShouldBeNil)
+	})
+	Convey("Test CommitEvent of MongoEventWriter", t, func() {
+		mng := NewMongoEventWriter()
+		So(mng, ShouldNotBeNil)
+		err := mng.Dial("mongodb://127.0.0.1/test")
+		So(err, ShouldBeNil)
+		mng.SetEventStore("test", "events")
+		err = mng.CommitEvent("{\"test\":\"value\"}")
+		So(err, ShouldBeNil)
+		err = mng.CommitEvent("{\"array\":[\"123\",\"345\", 45, 3445.456]}")
+		So(err, ShouldBeNil)
+	})
+	Convey("Test EventReader", t, func() {
+		ev := NewPositiveEventReader()
+		So(ev, ShouldNotBeNil)
+		err := ev.Dial("test")
+		So(err, ShouldBeNil)
+		err = ev.ReadEvents("fake ID", nil)
+		So(err, ShouldBeNil)
+		ev.Close()
 	})
 	Convey("Run loadSystemState", t, func() {
 		ch := make(chan string)
@@ -31,12 +84,27 @@ func TestCurrentState(t *testing.T) {
 		So(sysState.scalar.VarID, ShouldEqual, 1)
 		So(sysState.scalar.TimePoint, ShouldEqual, 1)
 	})
-	Convey("Run main", t, func() {
-		main()
-		So(sysState, ShouldNotBeNil)
-		So(sysState.scalar.Value, ShouldEqual, 1.0)
-		So(sysState.scalar.BoxID, ShouldEqual, 1)
-		So(sysState.scalar.VarID, ShouldEqual, 1)
-		So(sysState.scalar.TimePoint, ShouldEqual, 1)
+	Convey("ReadEvents from the database", t, func() {
+		ch := make(chan string)
+		mng := NewMongoEventReader()
+		So(mng, ShouldNotBeNil)
+		err := mng.Dial("mongodb://127.0.0.1/test")
+		So(err, ShouldBeNil)
+		mng.SetEventStore("test", "events")
+		err = mng.ReadEvents("", ch)
+		So(err, ShouldBeNil)
+		for s := range ch {
+			So(s, ShouldNotEqual, "")
+		}
 	})
+	/*
+		Convey("Run main", t, func() {
+			main()
+			So(sysState, ShouldNotBeNil)
+			So(sysState.scalar.Value, ShouldEqual, 1.0)
+			So(sysState.scalar.BoxID, ShouldEqual, 1)
+			So(sysState.scalar.VarID, ShouldEqual, 1)
+			So(sysState.scalar.TimePoint, ShouldEqual, 1)
+		})
+	*/
 }
