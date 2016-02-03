94a9947cefb26d79f09895a6e3499d815f26bd09
Sysoev, Vladimir <vladimir.sysoev@sarov-itc.ru>
2016-02-03 17:04:26 +0300

    Missed test file to. added tests for currentsrv. fixed issue with unexpected mongo session closing

---
+++ currentsrv/currentsrv_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/vsysoev/goeventstore/evstore"
+)
+
+const (
+	mongoURL string = "mongodb://127.0.0.1"
+)
+
+func TestCurrentScalarState(t *testing.T) {
+	Convey("When commit current message to database", t, func() {
+		ev, err := evstore.Dial(mongoURL, "test", "events")
+		So(err, ShouldBeNil)
+		So(ev, ShouldNotBeNil)
+
+		ch, err := ev.Listenner().Subscribe("")
+		So(ch, ShouldNotBeNil)
+	Loop:
+		for {
+			select {
+			case <-ch:
+				//			fmt.Println(msg)
+				break
+			case <-time.After(time.Second):
+				break Loop
+			}
+		}
+		fmt.Println("All messages read")
+		for i := 1; i < 10; i++ {
+			timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+			log.Println(timestamp)
+			rand.Seed(time.Now().UTC().UnixNano())
+			boxID := strconv.Itoa(rand.Intn(100))
+			varID := strconv.Itoa(rand.Intn(200))
+			val := strconv.FormatFloat(rand.NormFloat64(), 'f', 2, 64)
+			sendMsg := "{\"datestamp\":\"" + timestamp + "\", \"box_id\": " + boxID + ", \"var_id\": " + varID + ", \"value\": " + val + "}"
+			err = ev.Committer().SubmitEvent("123", "scalar", sendMsg)
+			So(err, ShouldBeNil)
+			msg := ""
+			select {
+			case msg = <-ch:
+				break
+			case <-time.After(time.Second * 10):
+				msg = "{\"event\":{\"error\": \"This is fucking shit error\"}}"
+				break
+			}
+			var msgJSON map[string]interface{}
+			err = json.Unmarshal([]byte(msg), &msgJSON)
+			So(msgJSON["event"].(map[string]interface{})["datestamp"].(string), ShouldEqual, timestamp)
+
+		}
+		ev.Close()
+	})
+}
