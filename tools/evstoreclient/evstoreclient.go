2d7e7fa9927177e47a94d20bd91f6f763bb77fb2
Sysoev, Vladimir <vladimir.sysoev@sarov-itc.ru>
2016-02-07 16:50:36 +0300

    implemented -msg key

--- tools/evstoreclient/evstoreclient.go
+++ tools/evstoreclient/evstoreclient.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"flag"
-	"io/ioutil"
+	"fmt"
+	"io"
 	"log"
 	"os"
 
 	"github.com/vsysoev/goeventstore/evstore"
 )
 
-func Send2EventStore(jsonString []byte, evStore *evstore.Connection) {
-	js := make(map[string]interface{})
-	err := json.Unmarshal(jsonString, &js)
-	if err != nil {
-		log.Panicln(err)
-		return
+func Send2EventStore(r *bufio.Reader, evStore *evstore.Connection) {
+	for {
+		s, _, err := r.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return
+		}
+		fmt.Println(string(s))
+		err = evStore.Committer().SubmitEvent("", "scalar", string(s))
+		if err != nil {
+			log.Println(err)
+		}
 	}
 	return
 }
@@ -25,23 +34,34 @@ func main() {
 		uri            string
 		dbName         string
 		collectionName string
+		msg            string
 	)
 	flag.StringVar(&fileName, "f", "", "JSON file name with events")
 	flag.StringVar(&uri, "s", "", "Event store URI")
 	flag.StringVar(&dbName, "db", "", "Database name")
 	flag.StringVar(&collectionName, "c", "", "Collection name")
+	flag.StringVar(&msg, "msg", "", "Message to send to database")
 	flag.Parse()
-	if _, err := os.Stat(fileName); err != nil {
-		log.Panicln(err)
-	}
-	jsonStrings, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		log.Panicln(err)
-	}
 	evStore, err := evstore.Dial(uri, dbName, collectionName)
 	if err != nil {
 		log.Panicln(err)
 	}
-	Send2EventStore(jsonStrings, evStore)
+	if fileName != "" {
+		if _, err := os.Stat(fileName); err != nil {
+			log.Panicln(err)
+		}
+		f, err := os.Open(fileName)
+		if err != nil {
+			log.Panicln(err)
+		}
+		r := bufio.NewReader(f)
+		Send2EventStore(r, evStore)
+	}
+	if msg != "" {
+		err = evStore.Committer().SubmitEvent("", "scalar", msg)
+		if err != nil {
+			log.Panicln(err)
+		}
+	}
 	evStore.Close()
 }
