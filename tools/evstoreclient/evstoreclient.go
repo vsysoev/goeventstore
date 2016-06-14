852e9afc756f8ec084f6c68059442e5ae5b151fc
Sysoev, Vladimir <vladimir.sysoev@gmail.com>
2016-06-14 10:10:14 +0300

    added evstoreclient to generate values

--- tools/evstoreclient/evstoreclient.go
+++ tools/evstoreclient/evstoreclient.go
@@ -6,11 +6,17 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"os"
+	"strconv"
+	"time"
+
+	"golang.org/x/net/context"
 
 	"github.com/vsysoev/goeventstore/evstore"
 )
@@ -32,6 +38,89 @@ func Send2EventStore(r *bufio.Reader, evStore *evstore.Connection) {
 	}
 	return
 }
+func genValue(mode map[string]interface{}) (float64, map[string]interface{}) {
+
+	switch mode["type"].(string) {
+	case "random":
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		return r.Float64() * mode["max"].(float64), mode
+	case "meandr":
+		if val, ok := mode["value"]; ok {
+			if val == mode["min"] {
+				mode["value"] = mode["max"]
+			} else {
+				mode["value"] = mode["min"]
+			}
+		} else {
+			mode["value"] = mode["min"]
+		}
+		return mode["value"].(float64), mode
+	case "saw":
+		if val, ok := mode["value"]; ok {
+			if val.(float64) < mode["max"].(float64) {
+				mode["value"] = val.(float64) + 1
+			} else {
+				mode["value"] = mode["min"]
+			}
+		} else {
+			mode["value"] = mode["min"]
+		}
+		return mode["value"].(float64), mode
+	}
+	return 0.0, nil
+}
+func genData(ctx context.Context, modeString []byte, chanOutput chan string) {
+	var (
+		mode  map[string]interface{}
+		value float64
+	)
+	err := json.Unmarshal(modeString, &mode)
+	if err != nil {
+		log.Panicln("Error in generation mode:", string(modeString))
+	}
+	log.Println(mode)
+	m := mode["mode"].(map[string]interface{})
+Loop:
+	for {
+		select {
+		case <-ctx.Done():
+			break Loop
+		case <-time.After(time.Duration(mode["delay"].(float64)) * time.Millisecond):
+			value, m = genValue(m)
+			event := "{\"datastamp\":" + strconv.FormatInt(time.Now().Unix(), 10)
+			event = event + ", \"box_id\":" + strconv.FormatInt(int64(mode["box_id"].(float64)), 10)
+			event = event + ", \"var_id\":" + strconv.FormatInt(int64(mode["var_id"].(float64)), 10)
+			event = event + ", \"value\":" + strconv.FormatFloat(value, 'f', 5, 32) + "}"
+			chanOutput <- event
+			break
+		}
+	}
+}
+func GenDataFromGenFile(ctx context.Context, genFile *bufio.Reader, evStore *evstore.Connection) {
+	chanInput := make(chan string, 1)
+	for {
+		s, _, err := genFile.ReadLine()
+		if err == io.EOF {
+			log.Println("EOF")
+			break
+		}
+		if err != nil {
+			log.Panicln(err)
+		}
+		go genData(ctx, s, chanInput)
+	}
+	log.Println("Waiting for Ctrl-C")
+Loop:
+	for {
+		select {
+		case <-ctx.Done():
+			break Loop
+		case msg := <-chanInput:
+			log.Println(msg)
+			evStore.Committer().SubmitEvent("", "scalar", msg)
+		}
+	}
+}
 func main() {
 	var (
 		fileName       string
@@ -39,12 +128,14 @@ func main() {
 		dbName         string
 		collectionName string
 		msg            string
+		genFile        string
 	)
 	flag.StringVar(&fileName, "f", "", "JSON file name with events")
 	flag.StringVar(&uri, "s", "", "Event store URI")
 	flag.StringVar(&dbName, "db", "", "Database name")
 	flag.StringVar(&collectionName, "c", "", "Collection name")
 	flag.StringVar(&msg, "msg", "", "Message to send to database")
+	flag.StringVar(&genFile, "gen", "", "Genfile with information")
 	flag.Parse()
 	evStore, err := evstore.Dial(uri, dbName, collectionName)
 	if err != nil {
@@ -66,6 +157,18 @@ func main() {
 		if err != nil {
 			log.Panicln(err)
 		}
+	}
+	if genFile != "" {
+		if _, err := os.Stat(genFile); err != nil {
+			log.Panicln(err)
+		}
+		f, err := os.Open(genFile)
+		if err != nil {
+			log.Panicln(err)
+		}
+		r := bufio.NewReader(f)
+		log.Println("Start GenDataFromGenFile")
+		GenDataFromGenFile(context.Background(), r, evStore)
 	}
 	evStore.Close()
 }
